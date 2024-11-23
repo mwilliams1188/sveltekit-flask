4ce6fcb4269125bed7aa7a7daa1d6645616a96c6
Mike Williams <mike@buildlab.co>
2024-11-22 19:12:28 -0500

    make build for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	});
 
 	const fetchData = async () => {
-		const res = await fetch('/api/hello');
+		const res = await fetch('/api/python');
 		const json = await res.json();
 		message = json.message;
 	};
@@ -27,11 +27,11 @@
 		<h1 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
 			{!message ? 'Loading...' : message}
 		</h1>
-		<a
+		<!-- <a
 			href="/api/hello"
 			class="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
 		>
 			View the API call
-		</a>
+		</a> -->
 	</div>
 </section>
